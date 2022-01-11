5f3422c477e930e763d148783ac1e8476585448e
Ayaal Olesov <ayal.olesov@mytona.com>
2022-01-11 23:01:28 +0300

    check github actions

--- homework0/src/main.cpp
+++ homework0/src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Version: " << version() << "\n";
+	std::cout << "Hello, world!\n";
 	return 0;
 }
